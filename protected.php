67870401c773f5a7652d76da8083f2dacbe92b43
Michal Lisiecki <lisiecki.m@gmail.com>
2013-10-24 23:23:04 +0200

    Dalszy rozwój

--- protected.php
+++ protected.php
@@ -1,5 +1,4 @@
 <?php
-
 include_once './users/LoginManager.php';
 $loginManager = new LoginManager;
 if (!$loginManager->isLoggedIn()) {
--- template/menu.php
+++ template/menu.php
@@ -1,5 +1,7 @@
 <div class="menu">
-    <div class="menu_entry">Wpis1</div>
-    <div class="menu_entry">Wpis2</div>
+    <div class="menu_entry"><a href="index.php">Strona główna</a></div>
+    <div class="menu_entry"><a href="register.php">Zarejestruj się</a></div>
+    <div class="menu_entry"><a href="protected.php">Informacje o projekcie(*)</a></div>
+    <div class="menu_entry"><a href="phpinfo.php">PHP Info</a></div>
 </div>
 
